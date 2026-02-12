7332b33c8cf1951f87316ba45ced95e9524eaeb1
Harshad811 <harshadkrishnauppalapati@gmail.com>
2026-02-12 23:36:42 +0530

    Initial Terraform Azure free resources setup

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "Central India"
+}
+
+variable "prefix" {
+  description = "Resource name prefix"
+  type        = string
+  default     = "free"
+}
